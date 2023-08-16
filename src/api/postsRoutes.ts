ebeaa7738173ac3ba4279f5c4648a886a90b1c39
kejuki <kerkkokiikeri97@gmail.com>
2023-08-16 14:58:59 +0300

    new route for dummy posts in profile

--- src/api/postsRoutes.ts
+++ src/api/postsRoutes.ts
@@ -11,6 +11,11 @@ export function getPostsPaginated({ pageParam = 1 }): Promise<any>{
     .then(res => {return res.data} )
 }
 
+export function getPostsPaginatedByuserId({ pageParam = 1 }, userId:string): Promise<any>{
+  return axios.get(`http://localhost:3001/igposts/${userId}?page=${pageParam}`)
+    .then(res => {return res.data} )
+}
+
 export function getPostsById(id: string): Promise<any>{
   return axios.get(`http://localhost:3001/igposts/${id}`)
     .then(res => { return res.data })
