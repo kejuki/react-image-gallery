d7a7e905e3bc32d38ac7e5ce829ea291b3153178
kejuki <kerkkokiikeri97@gmail.com>
2023-08-23 14:59:57 +0300

    profile page prototyping

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,10 @@
 import { getUser } from "../api/profileRoutes"
 import { useQuery, useInfiniteQuery } from 'react-query'
+import { getPostsPaginatedByuserId } from "../api/postsRoutes"
 import testavatar from "../assets/testavatar.jpg"
+import test from "../assets/test.jpg"
 import '../css/profile.css'
+import { Post } from "../types/post"
 
 function Profile(){
 
@@ -12,12 +15,24 @@ function Profile(){
     queryFn: () => getUser(userId),
   })
 
-  
-
-
+  const { 
+    status,
+    error,
+    data,
+    isFetching,
+    hasNextPage,
+    fetchNextPage,
+    } = useInfiniteQuery(
+      ["posts", userId], 
+      ({pageParam, queryKey}) => getPostsPaginatedByuserId({pageParam}, queryKey[1]), {
+      getNextPageParam: (lastPage, pages) => {
+        if(lastPage.page < lastPage.totalPages) return lastPage.page + 1
+        return false
+      }
+  })
 
   if (user.status === "loading") return (<div>loading...</div>)
-  console.log(user)
+
   return(
     <div id="profile-wrapper">
       <div className="profile-header">
@@ -35,10 +50,26 @@ function Profile(){
             <div className="profile-stat">666 following</div>
           </div>
           <div className="profile-realname">{user?.data?.realname}</div>
-          <div className="prfile-description">{user?.data?.description}</div>
+          <div className="profile-description">{user?.data?.description}</div>
         </div>
       </div>
-
+      {status === "loading" ? <>loading...</> : 
+      <div className="profile-posts">
+        {data?.pages?.map((page, i) => (
+          <div key={i} className="profile-posts-cotainer">
+            {page?.posts?.map((post: Post)=>(
+              <article key={post.postId} className="profile-post">
+                <div>
+                  <img className="profile-post-image" src={test}></img>
+                </div>
+              </article>
+            ))}
+          </div>
+        ))}
+      {isFetching && <p>Loading ...</p>}
+      {hasNextPage && <button onMouseOver={fetchNextPage}>Load More</button>}
+      </div>
+      }
     </div>
   )
 }
